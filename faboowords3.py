5eb1e26e9abacacd1db1a306a64d27ba4ff6390a
EXTMASZL <deshmukh259.2010@gmail.com>
2019-12-18 22:47:22 +0000

    test 2

--- faboowords3.py
+++ faboowords3.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
          '1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679 8214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196 104683731294243150']
     # z = []
     # coun = int(input())
-    # if coun <1 and coun >100:
+    # if coun < 1 and coun > 100:
     #     exit(0)
     # for zz in range(coun):
     #     z.append(input())
@@ -14,31 +14,22 @@ if __name__ == "__main__":
     x = 0
     len1 = len(z)
     while x < (len1):
-
         bb = z[x].split(' ')
         x += 1
         a = bb[0];
-        if(len(a) <1 and len(a)>100):
-            exit(0)
-
         b = bb[1];
-        if(len(b) <1 and len(b)>100):
-            exit(0)
         n = int(float(bb[2]));
-        if n< 1 and (1<<100)+1 > n:
-            exit(0)
-        # q = int(n / 7);
-        q = n
-        c = '';
-        i = 0
-
-        for i in range(q - 1):
-            if len(c) < (n + 1):
-                c = a + b;
-                a = b;
-                b = c;
-
-        # print(c)
-        n_ = n - 1
-
-        print(c[n_])
+        le = len(a)
+        f = None
+        n/2
+        if (int(n / le)) % 2 == 0:
+            f = b
+        else:
+            f = a
+        nr = 0
+        cv = 0
+        bb = 0
+        # while nr < n:
+        #     nr = nr * le
+        pos = int(n - (int(n / le) * le))
+        print(f[pos - 1])
---
+++ faboowords4.py
@@ -0,0 +1,62 @@
+if __name__ == "__main__":
+    # for v in range(int(int(input()))):
+    #     c[v] = input()
+    # x = 0
+    z = ['1415926535 8979323846 35'
+     ,
+     '1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679 8214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196 104683731294243150']
+    # z = []
+    # coun = int(input())
+    # if coun <1 and coun >100:
+    #     exit(0)
+    # for zz in range(coun):
+    #     z.append(input())
+
+    x = 0
+    len1 = len(z)
+    while x < (len1):
+
+        bb = z[x].split(' ')
+        x += 1
+        a = 'a';
+        a1 = bb[0];
+        if (len(a) < 1 and len(a) > 100):
+            exit(0)
+
+        b1 = bb[1];
+        b = 'b';
+        if (len(b) < 1 and len(b) > 100):
+            exit(0)
+        n = int(float(bb[2]));
+        if n < 1 and (1 << 100) + 1 > n:
+            exit(0)
+        # q = int(n / 7);
+        q = n
+        c = '';
+        i = 0
+        nc = 0
+
+        nal = len(a1)
+        nbl = len(b1)
+        tot = 0
+        while tot <= q:
+            ac = c.count('a')
+            bc = c.count('b')
+            tot += (nal * ac) + (nbl * bc)
+            c = a + b;
+            a = b;
+            b = c;
+            # print (c)
+        a = None
+        b = None
+        last1 = c[-1]
+        c= None
+
+        print ('tot ', tot)
+        print ('q-tot', q - tot)
+        print ('q-tot-nb ', q - tot - nbl)
+        print ('b1- ', b1)
+        if last1 == 'a':
+            print(a1[q - tot + nal-2])
+        if last1 == 'b':
+            print(b1[q - tot + nbl-2])
